80885ffdde7247c76d9e16295cc3de6c76b917a8
ztlsir <584821699@qq.com>
2020-06-05 15:00:29 +0800

    add BaseAggregate

---
+++ education-shared-model/src/main/java/com/ztlsir/shared/model/BaseAggregate.java
@@ -0,0 +1,27 @@
+package com.ztlsir.shared.model;
+
+import com.ztlsir.shared.event.DomainEvent;
+
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
+
+public abstract class BaseAggregate {
+    private List<DomainEvent> _events;
+
+    protected final void raiseEvent(DomainEvent event) {
+        get_events().add(event);
+    }
+
+    final void clearEvents() {
+        get_events().clear();
+    }
+
+    final List<DomainEvent> get_events() {
+        if (_events == null) {
+            _events = newArrayList();
+        }
+        return _events;
+    }
+
+}
--- education-shared-model/src/main/java/com/ztlsir/shared/model/BaseRepository.java
+++ education-shared-model/src/main/java/com/ztlsir/shared/model/BaseRepository.java
@@ -1,9 +1,20 @@
 package com.ztlsir.shared.model;
 
-public abstract class BaseRepository<TAggregate> {
+import com.ztlsir.shared.event.DomainEventSender;
+
+public abstract class BaseRepository<TAggregate extends BaseAggregate> {
+    private DomainEventSender sender;
+
+    protected BaseRepository(DomainEventSender sender) {
+        this.sender = sender;
+    }
 
     public final void save(TAggregate aggregate) {
         doSave(aggregate);
+        aggregate.get_events().forEach(event -> {
+            sender.send(event);
+        });
+        aggregate.clearEvents();
     }
 
     protected abstract void doSave(TAggregate aggregate);
