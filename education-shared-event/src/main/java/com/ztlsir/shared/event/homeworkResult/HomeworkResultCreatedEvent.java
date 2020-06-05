482de3cefbefbeb5c45ca551ade06af12369608f
ztlsir <584821699@qq.com>
2020-06-05 11:19:22 +0800

    add HomeworkResultCreatedEvent

---
+++ education-shared-event/src/main/java/com/ztlsir/shared/event/homeworkResult/HomeworkResultCreatedEvent.java
@@ -0,0 +1,13 @@
+package com.ztlsir.shared.event.homeworkResult;
+
+import com.ztlsir.shared.event.DomainEvent;
+import lombok.Getter;
+
+@Getter
+public class HomeworkResultCreatedEvent extends DomainEvent {
+    private String homeworkResultId;
+
+    public HomeworkResultCreatedEvent(String homeworkResultId) {
+        this.homeworkResultId = homeworkResultId;
+    }
+}
